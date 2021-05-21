4fd4ff562895128900e27b1bda8d51ee884c31e0
Eric Magalhaes <15831837+eric-souzams@users.noreply.github.com>
2021-05-21 18:37:32 -0300

    wip: /clientes endpoint to list all clients

--- src/main/java/com/api/workslog/api/controller/ClienteController.java
+++ src/main/java/com/api/workslog/api/controller/ClienteController.java
@@ -1,31 +1,24 @@
 package com.api.workslog.api.controller;
 
 import com.api.workslog.domain.model.Cliente;
+import com.api.workslog.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
+    //@Autowired
+    private ClienteRepository clienteRepository;
+
     @GetMapping("/clientes")
     public List<Cliente> list() {
-
-        var c1 = new Cliente();
-        c1.setId(1L);
-        c1.setEmail("junio1@gmail.com");
-        c1.setNome("Junio Silva");
-        c1.setTelefone("999999999");
-
-        var c2 = new Cliente();
-        c2.setId(2L);
-        c2.setEmail("pedrin@gmail.com");
-        c2.setNome("Pedro Souza");
-        c2.setTelefone("8888999779");
-
-        return Arrays.asList(c1, c2);
+        return clienteRepository.findAll();
     }
 
 }
