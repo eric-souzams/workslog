225fde7fd2f29e3f30f17d9624b2320921526da2
Eric Magalhaes <15831837+eric-souzams@users.noreply.github.com>
2021-05-22 19:01:46 -0300

    wip: simplification with model mapper on the objects transform

--- src/main/java/com/api/workslog/api/model/ClienteResumoModel.java
+++ src/main/java/com/api/workslog/api/model/ClienteResumoModel.java
@@ -1,4 +1,13 @@
 package com.api.workslog.api.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ClienteResumoModel {
+
+    private Long id;
+    private String nome;
+
 }
--- src/main/java/com/api/workslog/domain/model/Cliente.java
+++ src/main/java/com/api/workslog/domain/model/Cliente.java
@@ -11,9 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import javax.validation.groups.Default;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
@@ -21,7 +19,6 @@ import javax.validation.groups.Default;
 @Entity
 public class Cliente {
 
-    @NotNull(groups = ValidationGroups.ClienteId.class)
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
